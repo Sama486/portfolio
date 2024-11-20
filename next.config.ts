66c76aaacdf10df96407c534cb7cffa8ca853ae3
Karim Benziane <k.benziane@web.de>
2024-11-20 14:54:02 +0100

    BK

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: isProduction ? '/portfolio' : '',
+  assetPrefix: isProduction ? '/portfolio/' : '',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
